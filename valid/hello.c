4d4ae094b9693d7ddbf4601ecaca37d396904d0e
Jack Grahl <jack.grahl@gmail.com>
2021-03-12 23:47:41 +0000

    C code, assembly, object file, Linux binary.

---
+++ valid/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv){
+    int x = argc;
+    int y = 9;
+    printf("This is a sample C file\n");
+    return 0;
+}
---
+++ valid/hello.s
@@ -0,0 +1,51 @@
+	.arch armv6
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"hello.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"This is a sample C file\000"
+	.text
+	.align	2
+	.global	main
+	.arch armv6
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #16
+	str	r0, [fp, #-16]
+	str	r1, [fp, #-20]
+	ldr	r3, [fp, #-16]
+	str	r3, [fp, #-8]
+	mov	r3, #9
+	str	r3, [fp, #-12]
+	ldr	r0, .L3
+	bl	puts
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	pop	{fp, pc}
+.L4:
+	.align	2
+.L3:
+	.word	.LC0
+	.size	main, .-main
+	.ident	"GCC: (Raspbian 8.3.0-6+rpi1) 8.3.0"
+	.section	.note.GNU-stack,"",%progbits
